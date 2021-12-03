a2d35d2d85c474b9e5a843a0ef210a1e9f291d11
Nico Arato <nico.arato@gmail.com>
2021-12-03 00:08:55 -0300

    Se agrega darkmode e iconos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
 
 
@@ -11,7 +11,7 @@ import { Farmacia } from '../interfaces/farmacia.interface';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, AfterViewInit {
 
   mapa: any;
   LngLat: Mapboxgl.LngLat;
@@ -19,94 +19,154 @@ export class Tab2Page implements OnInit {
   visibles = false;
   markers: Mapboxgl.Marker[] = [];
   estilo = 'primary';
-  constructor( private farmaciaService: FarmaciasService ) {}
+  estiloMapa = '';
+  constructor(private farmaciaService: FarmaciasService) {
+
+  }
 
   ngOnInit() {
     // evita la carga con defectos del mapa
     setTimeout(() => this.cargarMapa(), 100);
 
-    this.cargarFarmacias();
+    setTimeout(() =>
+      this.cargarFarmacias(), 1000);
+
+    setTimeout(() =>
+      this.mapa.resize(), 1000);
+
+
 
   }
 
-  cargarMapa(){
+  ngAfterViewInit() {
+
+  }
+
+
+  cargarMapa() {
+    const hoy = new Date();
+    const hora = hoy.getHours();
+    let estiloMapa;
+    if (hora < 19 && hora > 7) {
+      estiloMapa = 'mapbox://styles/mapbox/light-v10';
+    } else {
+      estiloMapa = 'mapbox://styles/mapbox/dark-v10';
+    }
     // inicializa el mapa
     (Mapboxgl as any).accessToken = environment.mapboxKey;
     this.mapa = new Mapboxgl.Map({
       container: 'mapamapbox', // container id
-      style: 'mapbox://styles/mapbox/light-v10',
+      // style: 'mapbox://styles/mapbox/light-v10',
+      style: estiloMapa,
       center: [-60.707367, -31.6251114], // starting position lng, lat
-      zoom: 13 // starting zoom
+      zoom: 13, // starting zoom
+      pitch: 15
     });
 
     this.mapa.addControl(new Mapboxgl.NavigationControl());
     this.mapa.addControl(new Mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
     }));
+
+    this.mapa.on('load', () => {
+      this.mapa.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+          'fill-extrusion-color': '#aaa',
+
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': 0.6
+        }
+      });
+    })
   }
 
 
-    // carga el pedido por http
-    cargarFarmacias(event?) {
+  // carga el pedido por http
+  cargarFarmacias(event?) {
     this.farmaciaService.getFarmacias()
-    .subscribe(res => {
-      if (event) {
-        event.target.complete();
-      }
-      this.farmacias = res;
-      if (this.farmacias.length > 0) {
-        console.log('carga exitosa -> filas: ' , this.farmacias.length);
-        return true;
+      .subscribe(res => {
+        if (event) {
+          event.target.complete();
+        }
+        this.farmacias = res;
+        if (this.farmacias.length > 0) {
+          console.log('carga exitosa -> filas: ', this.farmacias.length);
+          return true;
         } else {
           console.log('problemas para cargar farmacias');
           return false;
         }
       });
-    }
+  }
 
-    // muestra todas las farmacias cargadas
-    mostrarFarmacias() {
-      if (!this.visibles){
-        this.visibles = true;
-        const farms: Farmacia[] = this.farmacias;
-        farms.forEach(farmacia => {
-          this.cargarFarmacia(farmacia);
-        });
-      } else {
-        this.visibles = false;
-        this.quitarFarmacias();
-      }
-      this.estilo = ( !this.visibles ? 'primary' : 'danger');
+  // muestra todas las farmacias cargadas
+  mostrarFarmacias() {
+    if (!this.visibles) {
+      this.visibles = true;
+      const farms: Farmacia[] = this.farmacias;
+      farms.forEach(farmacia => {
+        this.cargarFarmacia(farmacia);
+      });
+    } else {
+      this.visibles = false;
+      this.quitarFarmacias();
     }
+    this.estilo = (!this.visibles ? 'primary' : 'danger');
+  }
 
-    // carga una farmacia en el mapa
-    cargarFarmacia(farmacia: Farmacia) {
-      const lng = farmacia.coords[1];
-      const lat = farmacia.coords[0];
-      const marker = new Mapboxgl.Marker({
-        color: '#42d77d',
-      })
+  // carga una farmacia en el mapa
+  cargarFarmacia(farmacia: Farmacia) {
+    const lng = farmacia.coords[1];
+    const lat = farmacia.coords[0];
+    const marker = new Mapboxgl.Marker({
+      color: '#42d77d',
+    })
       .setLngLat([lng, lat])
       .setPopup(new Mapboxgl.Popup({ closeOnClick: false })
-                .setLngLat([lng, lat])
-                .setHTML(`<p><strong>${farmacia.nombre}</strong>
+        .setLngLat([lng, lat])
+        .setHTML(`<p><strong>${farmacia.nombre}</strong>
                           <br>${farmacia.telefono}</p>`))
       .addTo(this.mapa);
-      // carga array de marcadores.
-      this.markers.push(marker);
+    // carga array de marcadores.
+    this.markers.push(marker);
 
-    }
+  }
 
-    quitarFarmacias() {
-      this.markers.forEach(farmacia => {
-        farmacia.remove();
-      });
-      this.markers = [];
-      console.log('cantidad de elemntos en markers: ', this.markers.length);
-    }
+  quitarFarmacias() {
+    this.markers.forEach(farmacia => {
+      farmacia.remove();
+    });
+    this.markers = [];
+    console.log('cantidad de elemntos en markers: ', this.markers.length);
+  }
 
 
 
